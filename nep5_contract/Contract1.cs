3cd2ea02dc2555d9dc79bdc746ffcf1c6ef12197
lights <lightsever@hotmail.com>
2017-12-26 08:40:02 +0800

    添加一个nep5合约

---
+++ nep5_contract/Contract1.cs
@@ -0,0 +1,106 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.ComponentModel;
+using System.Numerics;
+
+namespace Nep5_Contract
+{
+    public class ContractNep5 : SmartContract
+    {
+        public static object Main(string method, object[] args)
+        {
+            var magicstr = "2017-12-26";
+
+            if (Runtime.Trigger == TriggerType.Verification)//取钱才会涉及这里
+            {
+                return Runtime.CheckWitness(SuperAdmin);
+            }
+            else if (Runtime.Trigger == TriggerType.Application)
+            {
+                //this is in nep5
+                if (method == "totalSupply") return TotalSupply();
+                if (method == "name") return Name();
+                if (method == "symbol") return Symbol();
+                if (method == "decimals") return Decimals();
+                if (method == "balanceOf")
+                {
+                    if (args.Length != 1) return 0;
+                    byte[] account = (byte[])args[0];
+                    return BalanceOf(account);
+                }
+                if (method == "transfer")
+                {
+                    if (args.Length != 3) return false;
+                    byte[] from = (byte[])args[0];
+                    byte[] to = (byte[])args[1];
+                    BigInteger value = (BigInteger)args[2];
+                    return Transfer(from, to, value);
+                }
+                //this is add
+                if (method == "deploy")
+                {
+                    if (args.Length != 2) return false;
+                    byte[] admin = (byte[])args[0];
+                    BigInteger value = (BigInteger)args[1];
+                    return Deploy(admin,value);
+                }
+
+            }
+            return true;
+        }
+        public delegate void deleTransfer(byte[] from, byte[] to, BigInteger value);
+        [DisplayName("transfer")]
+        public static event deleTransfer Transferred;
+        public static readonly byte[] SuperAdmin = { 47, 60, 170, 33, 216, 40, 148, 2, 242, 150, 9, 84, 154, 50, 237, 160, 97, 90, 55, 183 };
+        private const ulong factor = 100000000;
+        public static BigInteger TotalSupply()
+        {
+            return Storage.Get(Storage.CurrentContext, "totalSupply").AsBigInteger();
+        }
+        public static string Name()
+        {
+            return "NEP5 Sample Coin";
+        }
+        public static string Symbol()
+        {
+            return "NEL";
+        }
+        public static byte Decimals()
+        {
+            return 8;
+        }
+        public static BigInteger BalanceOf(byte[] address)
+        {
+            return Storage.Get(Storage.CurrentContext, address).AsBigInteger();
+        }
+        public static bool Transfer(byte[] from, byte[] to, BigInteger value)
+        {
+            if (value <= 0) return false;
+            if (!Runtime.CheckWitness(from)) return false;
+            if (from == to) return true;
+            BigInteger from_value = Storage.Get(Storage.CurrentContext, from).AsBigInteger();
+            if (from_value < value) return false;
+            if (from_value == value)
+                Storage.Delete(Storage.CurrentContext, from);
+            else
+                Storage.Put(Storage.CurrentContext, from, from_value - value);
+            BigInteger to_value = Storage.Get(Storage.CurrentContext, to).AsBigInteger();
+            Storage.Put(Storage.CurrentContext, to, to_value + value);
+            Transferred(from, to, value);
+            return true;
+        }
+        //增发
+        public static bool Deploy(byte[] admin, BigInteger value)
+        {
+            if (value <= 0) return false;
+            if (!Runtime.CheckWitness(admin)) return false;
+
+            BigInteger total_supply = Storage.Get(Storage.CurrentContext, "totalSupply").AsBigInteger();
+            total_supply += value;
+            Storage.Put(Storage.CurrentContext, admin, value);
+            Storage.Put(Storage.CurrentContext, "totalSupply", total_supply);
+            return true;
+        }
+    }
+}
---
+++ plugin_whoami/WhoAmi.cs
@@ -0,0 +1,99 @@
+﻿using Neo;
+using Neo.Core;
+using Neo.SmartContract;
+using Neo.VM;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace plugin_whoami
+{
+    public partial class WhoAmi : Form
+    {
+        public WhoAmi()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var key = GetScriptHashFromAddress(textBox1.Text);
+            label1.Text = key.ToHexString();
+
+            var skey = new Neo.Core.StorageKey();
+            skey.Key = key;
+            skey.ScriptHash = UInt160.Parse(plugin_whoami.whoami_scripthash);
+
+            var item = Neo.Core.Blockchain.Default.GetStorageItem(skey);
+            this.listBox1.Items.Clear();
+            if (item == null)
+            {
+                this.listBox1.Items.Add("<nothing>");
+            }
+            else
+            {
+                this.listBox1.Items.Add("hexstr:" + item.Value.ToHexString());
+                var text = "";
+                try
+                {
+                    text = System.Text.Encoding.UTF8.GetString(item.Value);
+                }
+                catch
+                {
+
+                }
+                this.listBox1.Items.Add("as str:" + text);
+            }
+        }
+        static byte[] GetScriptHashFromAddress(string add)
+        {
+            //后四个自己是hash
+            //第一个是魔法数字
+            var bts = Neo.Cryptography.Base58.Decode(add);
+            return bts.Take(bts.Length - 4).Skip(1).ToArray();
+
+        }
+
+        int index = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (plugin_whoami.api.CurrentWallet == null)
+                return;
+            var count = plugin_whoami.api.CurrentWallet.GetAccounts().Count();//.GetKeys().Count();
+            if (index >= count) index = 0;
+            var key = plugin_whoami.api.CurrentWallet.GetAccounts().ToArray()[index].GetKey();
+            this.textBoxPubKey.Text = key.PublicKey.EncodePoint(true).ToHexString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var p1 = new Neo.SmartContract.ContractParameter(Neo.SmartContract.ContractParameterType.String)
+            {
+                Value = textBoxName.Text
+            };
+            var script = plugin_whoami.MakeAppCallScript(plugin_whoami.whoami_scripthash, this.textBoxPubKey.Text, p1);
+            plugin_whoami.CallScript(script);
+
+        }
+
+        private void textBoxPubKey_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            StackItem item;
+            if(plugin_whoami.notifys.TryDequeue(out item))
+            {
+                this.listBox2.Items.Add(item.GetArray()[0].GetByteArray().ToHexString());
+            }
+        }
+    }
+}
