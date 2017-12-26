ac1b0769e95ce9b2eaf0aa084153e1f1bdb6fd72
lights <lightsever@hotmail.com>
2017-12-26 23:59:53 +0800

    fix count

--- nep5_contract/Contract1.cs
+++ nep5_contract/Contract1.cs
@@ -54,37 +54,37 @@ namespace Nep5_Contract
             Transferred(from, to, value);
             return true;
         }
-        //增发货币，仅限超级管理员
-        public static bool Deploy(byte[] admin, BigInteger value)
-        {
-            if (value <= 0) return false;
-            if (!Runtime.CheckWitness(admin)) return false;
+        ////增发货币，仅限超级管理员
+        //public static bool Deploy(byte[] admin, BigInteger value)
+        //{
+        //    if (value <= 0) return false;
+        //    if (!Runtime.CheckWitness(admin)) return false;
 
-            BigInteger total_supply = Storage.Get(Storage.CurrentContext, "totalSupply").AsBigInteger();
-            BigInteger total_admin = Storage.Get(Storage.CurrentContext, admin).AsBigInteger();
-            total_supply += value;
-            total_admin += value;
-            Storage.Put(Storage.CurrentContext, admin, total_admin);
-            Storage.Put(Storage.CurrentContext, "totalSupply", total_supply);
-            return true;
-        }
-        //销毁货币，仅限超级管理员
-        public static bool Destory(byte[] admin, BigInteger value)
-        {
-            if (value <= 0) return false;
-            if (!Runtime.CheckWitness(admin)) return false;
+        //    BigInteger total_supply = Storage.Get(Storage.CurrentContext, "totalSupply").AsBigInteger();
+        //    BigInteger total_admin = Storage.Get(Storage.CurrentContext, admin).AsBigInteger();
+        //    total_supply += value;
+        //    total_admin += value;
+        //    Storage.Put(Storage.CurrentContext, admin, total_admin);
+        //    Storage.Put(Storage.CurrentContext, "totalSupply", total_supply);
+        //    return true;
+        //}
+        ////销毁货币，仅限超级管理员
+        //public static bool Destory(byte[] admin, BigInteger value)
+        //{
+        //    if (value <= 0) return false;
+        //    if (!Runtime.CheckWitness(admin)) return false;
 
-            BigInteger total_supply = Storage.Get(Storage.CurrentContext, "totalSupply").AsBigInteger();
-            BigInteger total_admin = Storage.Get(Storage.CurrentContext, admin).AsBigInteger();
+        //    BigInteger total_supply = Storage.Get(Storage.CurrentContext, "totalSupply").AsBigInteger();
+        //    BigInteger total_admin = Storage.Get(Storage.CurrentContext, admin).AsBigInteger();
 
-            if (value > total_admin) return false;
+        //    if (value > total_admin) return false;
 
-            total_supply -= value;
-            total_admin -= value;
-            Storage.Put(Storage.CurrentContext, admin, total_admin);
-            Storage.Put(Storage.CurrentContext, "totalSupply", total_supply);
-            return true;
-        }
+        //    total_supply -= value;
+        //    total_admin -= value;
+        //    Storage.Put(Storage.CurrentContext, admin, total_admin);
+        //    Storage.Put(Storage.CurrentContext, "totalSupply", total_supply);
+        //    return true;
+        //}
         public static object Main(string method, object[] args)
         {
             var magicstr = "2017-12-26";
